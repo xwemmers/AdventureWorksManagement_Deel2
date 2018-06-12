077d3ec39665445cb11d5179b2b741ad19125dba
xanderw <info@xanderwemmers.nl>
2018-06-12 15:25:59 +0200

    Initial commit

---
+++ AWM.DataLayer/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWM.DataLayer
+{
+    public class ProductRepository
+    {
+        AdventureWorksEntities ctx = new AdventureWorksEntities();
+
+        public ProductRepository()
+        {
+
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return ctx.Products.AsNoTracking().ToList();
+        }
+
+        public void SaveProduct(Product p)
+        {
+            ctx.Entry(p).State = EntityState.Modified;
+            ctx.SaveChanges();
+        }
+    }
+}
---
+++ AdventureWorksManagement/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AWM.BusinessLayer;
+using AWM.DataLayer;
+
+namespace AdventureWorksManagement
+{
+    class MainWindowViewModel
+    {
+        ProductLogic pl = new ProductLogic();
+
+        public MainWindowViewModel()
+        {
+            this.Products = pl.GetAllProducts();
+        }
+
+        public List<Product> Products { get; set; }
+    }
+}
